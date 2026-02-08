4dde2913ec38b1ce3ced42b5b61219e26928cb44
Jatin8copter <vjatin670@gmail.com>
2026-02-08 22:55:45 +0530

    add output block

--- AWS_VPC/sg.tf
+++ AWS_VPC/sg.tf
@@ -10,6 +10,13 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks = [ "0.0.0.0/0" ]
   }
 
+    ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
   egress {
     cidr_blocks = [ "0.0.0.0/0" ]
     protocol = "-1"
